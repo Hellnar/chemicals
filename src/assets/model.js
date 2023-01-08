1929401cb1a220eb0cae07aa277dfbdff93e893a
StasKlymenko <klymenko.stas@gmail.com>
2023-01-08 21:26:46 +0200

    Sketch for vitamins and additives models

---
+++ src/assets/model.js
@@ -0,0 +1,18 @@
+const model = {
+    name: String,
+    number: String,
+    function: String,
+    purpose: String,
+    status: {
+        us: Boolean,
+        eu: Boolean
+    },
+    formula: String 
+}
+
+const vitamins = [
+    {
+        name: "A",
+        
+    }
+]

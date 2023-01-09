7b001c2020dd87d21e00efb57b7c0a1b4ef62583
StasKlymenko <klymenko.stas@gmail.com>
2023-01-09 23:55:40 +0200

    Start filters section

--- src/assets/model.js
+++ src/assets/model.js
@@ -1,18 +1,18 @@
-const model = {
-    name: String,
-    number: String,
-    function: String,
-    purpose: String,
-    status: {
-        us: Boolean,
-        eu: Boolean
-    },
-    formula: String 
-}
+// const model = {
+//     name: String,
+//     number: String,
+//     function: String,
+//     purpose: String,
+//     status: {
+//         us: Boolean,
+//         eu: Boolean
+//     },
+//     formula: String 
+// }
 
-const vitamins = [
-    {
-        name: "A",
+// const vitamins = [
+//     {
+//         name: "A",
         
-    }
-]
+//     }
+// ]
